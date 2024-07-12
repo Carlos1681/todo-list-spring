226761c87016b67498c7e4f7de57a6d95661db91
Carlos1681 <spjcarlosh@gmail.com>
2024-07-11 22:55:53 -0300

    Estrutura de pacotes e primeiras classes

---
+++ src/main/java/com/desafio/todo/service/TodoService.java
@@ -0,0 +1,39 @@
+package com.desafio.todo.service;
+
+import com.desafio.todo.model.Todo;
+import com.desafio.todo.repository.TodoRepository;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TodoService {
+
+    private TodoRepository todoRepository;
+
+    public TodoService(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
+
+    public List<Todo> create(Todo todo){
+        todoRepository.save(todo);
+        return list();
+    }
+
+    public List<Todo> list(){
+        Sort sort = Sort.by("priority").descending()
+                .and(Sort.by("name").ascending());
+        return todoRepository.findAll(sort);
+    }
+
+    public List<Todo> update(Todo todo){
+        todoRepository.save(todo);
+        return list();
+    }
+
+    public List<Todo> delete(long id){
+        todoRepository.deleteById(id);
+        return list();
+    }
+}
